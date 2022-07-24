9d994407233a0887556b386ec5e7213131525b44
NSLyuma <iinokayo@mail.ru>
2022-07-24 14:32:09 +0300

    Added actions with posts (get, create, remove, update)

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 import express from "express";
-import jwt from "jsonwebtoken";
-import bcrypt from "bcrypt";
 import mongoose from "mongoose";
-import { registerValidation } from "./validations/auth.js";
-import { validationResult } from "express-validator";
-import UserModel from "./models/User.js";
+import {
+  registerValidation,
+  loginValidation,
+  postCreateValidation,
+} from "./validations.js";
 import checkAuth from "./utils/checkAuth.js";
+import * as UserController from "./controllers/UserController.js";
+import * as PostController from "./controllers/PostController.js";
 
 mongoose
   .connect(
@@ -19,106 +21,22 @@ const app = express();
 app.use(express.json()); //позволяет читать json
 
 //поиск пользователя в базе данных
-app.post("/auth/login", async (req, res) => {
-  try {
-    const user = await UserModel.findOne({ email: req.body.email });
-
-    if (!user) {
-      return res.status(404).json({ message: "Пользователь не найден" });
-    }
-
-    const isValidPass = await bcrypt.compare(
-      req.body.password,
-      user._doc.passwordHash
-    );
-
-    if (!isValidPass) {
-      return res.status(400).json({ message: "Неверный логин или пароль" });
-    }
-
-    const token = jwt.sign(
-      {
-        _id: user._id,
-      },
-      "secret123",
-      {
-        expiresIn: "30d",
-      }
-    );
-
-    const { passwordHash, ...userData } = user._doc;
-
-    res.json({ ...userData, token });
-  } catch (err) {
-    console.log(err);
-    res.status(500).json({
-      message: "Не удалось авторизоваться",
-    });
-  }
-});
-
+app.post("/auth/login", loginValidation, UserController.login);
 //создание пользователя
-app.post("/auth/register", registerValidation, async (req, res) => {
-  //если придёт запрос на /auth/register, мы проверяем, есть ли в registerValidation то, что мы хотим
-  //если есть, то выполняется функция ниже
-  try {
-    const errors = validationResult(req);
-    if (!errors.isEmpty()) {
-      return res.status(400).json(errors.array());
-    }
-
-    //3 строки ниже - шифрование пароля
-    const password = req.body.password;
-    const salt = await bcrypt.genSalt(10);
-    const hash = await bcrypt.hash(password, salt);
-
-    const doc = new UserModel({
-      email: req.body.email,
-      fullName: req.body.fullName,
-      avatarUrl: req.body.avatarUrl,
-      passwordHash: hash,
-    });
-
-    const user = await doc.save();
-
-    const token = jwt.sign(
-      {
-        _id: user._id,
-      },
-      "secret123",
-      { expiresIn: "30d" }
-    );
-
-    const { passwordHash, ...userData } = user._doc;
-
-    res.json({ ...userData, token });
-  } catch (err) {
-    console.log(err);
-    res.status(500).json({
-      message: "Не удалось зарегистрироваться",
-    });
-  }
-});
-
+app.post("/auth/register", registerValidation, UserController.register);
 //получение пользователем информации о себе
-app.get("/auth/me", checkAuth, async (req, res) => {
-  try {
-    const user = await UserModel.findById(req.userId);
+app.get("/auth/me", checkAuth, UserController.getMe);
 
-    if (!user) {
-      return res.status(404).json({ message: "Пользователь не найден" });
-    }
-
-    const { passwordHash, ...userData } = user._doc;
-
-    res.json(userData);
-  } catch (err) {
-    console.log(err);
-    res.status(500).json({
-      message: "Нет доступа",
-    });
-  }
-});
+//получение всех статей
+app.get("/posts", PostController.getAll);
+//получение одной статьи
+app.get("/posts/:id", PostController.getOne);
+//создание статьи
+app.post("/posts", checkAuth, postCreateValidation, PostController.create);
+//удаление статьи
+app.delete("/posts/:id", checkAuth, PostController.remove);
+//редактирование/обновление статьи
+app.patch("/posts/:id", checkAuth, PostController.update);
 
 //на какой порт прикрепить наше приложение (тут 4444)
 app.listen(4444, (err) => {
